ae786a5fda08fb919ded13139e4f6b877a16957d
PaoloDiBello <dibellopol@gmail.com>
2019-06-18 13:25:51 +0200

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -15,6 +15,21 @@ app.use(express.static(publicPath));
 io.on('connection', function (socket) {
     console.log('New user connected');
 
+    // socket.emit from admin Welcome to the chat app
+    // socket.broadcast.emit from Admin text New user joined
+
+    socket.emit('newMessage', {
+        text: 'Welcome to the Chat App!',
+        from: 'admin',
+        createdAt: new Date().getTime()
+    })
+
+    socket.broadcast.emit('newMessage', {
+        text: 'New user joined the Chat App!',
+        from: 'admin',
+        createdAt: new Date().getTime()
+    })
+
 
     socket.on('createMessage', (message) => {
         console.log('createMessage', message)
@@ -25,6 +40,15 @@ io.on('connection', function (socket) {
                 text: message.text,
                 createdAt: new Date().getTime()
             })
+
+        //     socket.broadcast.emit(
+        //         'newMessage',
+        //         {
+        //             from: message.from,
+        //             text: message.text,
+        //             createdAt: new Date().getTime()
+        //         })
+
     })
 
     socket.on('disconnect', function (socket) {
@@ -40,4 +64,4 @@ app.get('/', (req, res) => {
 
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`Server Running On Port ${PORT}`));
+server.listen(PORT, () => console.log(`Server Running On Port ${PORT}`))
